e09bf26e3aa8be511c167ab0bda75c161e471bf6
KnightVision90 <joseph.moraniii@gmail.com>
2017-09-09 15:48:23 -0400

    Fix linting
    Fix issues found while linting

--- classes.js
+++ classes.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
 
 const GuildAntiraidSettingsSchema = new Schema({
 	guildId: String,
 	channelId: String,
-	seconds: {type: Number, default: 10},
-	limit: {type: Number, default: 4}
+	seconds: { type: Number, default: 10 },
+	limit: { type: Number, default: 4 }
 });
 
 const AntiraidSettings = class antiraidSettings {
